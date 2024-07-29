898000323bcc935425859af6b11d1331779e8b17
AtaBlood <aatakanavci2000@gmail.com>
2024-07-29 16:14:17 +0300

    Update role_4led_nextion.ino

--- role_4led_nextion/role_4led_nextion.ino
+++ role_4led_nextion/role_4led_nextion.ino
@@ -1,78 +1,35 @@
-int a_role = 16;
-int b_role = 4;
-int c_role = 15;
-int d_role = 2;
+#include <EasyNextionLibrary.h>
 
-int a;
-int b;
-int c;
-int d;
+#define LED_PIN 2 // LED'in bağlı olduğu pin
 
-bool bir=false;
-bool iki=false;
-bool uc=false;
-bool dort=false;
+EasyNex myNex(Serial2);
 
-void setup(){
-  pinMode(a_role, OUTPUT);
-  pinMode(b_role, OUTPUT);
-  pinMode(c_role, OUTPUT;
-  pinMode(d_role, OUTPUT);
+// Buton değeri için global değişken
+int buttonState = 0;
 
+void setup() {
+  pinMode(LED_PIN, OUTPUT); // LED pinini çıkış olarak ayarla
+  digitalWrite(LED_PIN, LOW); // LED'i başlangıçta kapalı tut
+
+  Serial.begin(9600);
+  myNex.begin(9600);
 }
 
-void loop(){
-  a=digitalRead(a_role);
-  b=digitalRead(b_role);
-  c=digitalRead(c_role);
-  d=digitalRead(d_role);
+void loop() {
+  myNex.NextionListen(); // Nextion ekranından gelen verileri dinle
 
-  if(a==1 && b==0 && c==0 && d==0 && bir==false){
-    bir=true;
-    digitalWrite(a_role, 1);
-    delay(1000);
+  // Butonun değerini oku
+  buttonState = myNex.readNumber("bt1.val");
+
+  checkButtonPress();
+}
+
+void checkButtonPress() {
+  if (buttonState == 1) { // Butona basıldığında değer 1 olur
+    digitalWrite(LED_PIN, HIGH); // LED'i yak
+    delay(500); // LED'i bir süre yanık tut
+    digitalWrite(LED_PIN, LOW); // LED'i söndür
+    myNex.writeNum("bt1.val", 0); // Değeri sıfırla
+    buttonState = 0; // Global değişkeni sıfırla
   }
-  else if (a==1 && b==0 && c==0 && d==0 && bir=true) {
-    bir=false;
-    digitalWrite(a_role, 0);
-    delay(1000);
-  }
-  if(a==0 && b==1 && c==0 && d==0 && iki==false){
-    iki=true;
-    digitalWrite(b_role, 1);
-    delay(1000);
-  }
-  else if (a==0 && b==1 && c==0 && d==0 && iki=true) {
-    iki=false;
-    digitalWrite(b_role, 0);
-    delay(1000);
-  }
-  if(a==0 && b==0 && c==1 && d==0 && uc=false){
-    uc=true;
-    digitalWrite(c_role, 1);
-    delay(1000);
-  }
-  else if(a==0 && b==0 && c==1 && d==0 && uc=true){
-    uc=false;
-    digitalWrite(c_role, 0)
-    delay(1000);
-  }
-  if(a==0 && b==0 && c==0 && d==1 && dort=false){
-    dort=true;
-    digitalWrite(d_role,1);
-    delay(1000);
-  }
-  else if(a==0 && b==0 && c==0 && d==1 && dort=true){
-    dort=false;
-    digitalWrite(d_role,0);
-    delay(1000);
-  }
-
-
-
-
-
-
-
-
 }
